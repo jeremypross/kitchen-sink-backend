d735d4a12fdbf425c20bf99e24a87ec5d015d664
Jeremy Ross <jeremypross@gmail.com>
2017-03-29 11:08:16 -0400

    first commit -initial directory structure

---
+++ index.js
@@ -0,0 +1,37 @@
+const express        = require('express');
+const bodyParser     = require('body-parser');
+const logger         = require('morgan');
+const path           = require('path');
+const methodOverride = require('method-override');
+const session        = require('express-session');
+const expressJWT     = require('express-jwt');
+const jwt            = require('jsonwebtoken');
+const cors           = require('cors');
+const PORT           = process.env.PORT || 8000;
+const app     = express();
+
+// config cors
+app.use(cors());
+
+// config morgan
+app.use(logger('dev'));
+
+// config path
+app.use(express.static(path.join(__dirname, 'public')));
+
+// config body parser
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+
+// config ejs for mock views
+app.set('view engine', 'ejs');
+
+// config method-override
+app.use(methodOverride('_method'));
+
+// link to resources.js
+app.use('/', require('./resources'));
+
+app.listen(PORT, () => {
+  console.log('Server is listening on', PORT);
+});
